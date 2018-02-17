d19c6c25b5a90d0f302916db1686228aa9d32e53
Bazsmagister <bosmer@mailbox.hu>
2018-02-17 15:11:12 +0100

    both model belognsToMany

--- app/Destination.php
+++ app/Destination.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-    //
+    //public $timestamps = false;
+    protected $fillable= ['product_id','destination','target_person', 'quantity'];
+
+
+    //When defining many-to-many relationships, you use belongsToMany() and not hasMany. hasMany is used for one-to-many relationships.
+    public function product() 
+    {
+        //return  $this->belongsTo('App\Product', 'product_id');
+        //Remember, Eloquent will automatically determine the proper foreign key column on the Comment model. By convention, Eloquent will take the "snake case" name of the owning model and suffix it with _id. So, for this example, Eloquent will assume the foreign key on the Comment model is post_id.
+
+        return  $this->belongsToMany('App\Product');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //protected $table = 'products'; //do i need this?
+    //public $timestamps = false;
+    protected $fillable = ['name'];
+
+    public static $rules = array(
+        'name' => 'required',
+        
+    );
+
+    public function destinations()
+    {
+        return  $this -> belongsToMany('App\Destination');
+
+        
+
+    }
+
 }
