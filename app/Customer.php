74a3fce72360f3f92d44bc290295029e38409ef5
Bazsmagister <bosmer007@gmail.com>
2018-02-18 16:31:19 +0100

    customer

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    //public $timestamps = false;
+    protected $fillable= ['name','address','email'];
+
+
+    //When defining many-to-many relationships, you use belongsToMany() and not hasMany. hasMany is used for one-to-many relationships.
+    public function products() 
+    {
+       
+        //Remember, Eloquent will automatically determine the proper foreign key column on the Comment model. By convention, Eloquent will take the "snake case" name of the owning model and suffix it with _id. So, for this example, Eloquent will assume the foreign key on the Comment model is post_id.
+
+        return  $this->belongsToMany('App\Product');//->withPivot('destination_product')->withTimestamps();
+    }
+
+    public function destinations() 
+    {
+        
+        //Remember, Eloquent will automatically determine the proper foreign key column on the Comment model. By convention, Eloquent will take the "snake case" name of the owning model and suffix it with _id. So, for this example, Eloquent will assume the foreign key on the Comment model is post_id.
+
+        return  $this->belongsToMany('App\Desination');//->withPivot('destination_product')->withTimestamps();
+    }
+
+
 }
--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -10,7 +10,6 @@ $factory->define(App\Customer::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         //'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         //'remember_token' => str_random(10),
-
         'created_at'=> $faker->dateTimeBetween($startDate = '-5 years', $endDate = '-3 years', $timezone = null),
         'updated_at'=> $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = null),
     ];
