5f9bdc0c48cb134c6035327227bf244421232c34
Bazsmagister <bosmer@mailbox.hu>
2018-02-26 11:09:40 +0100

    yo

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,11 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Order::class, function (Faker $faker) {
     return [
-        //'customer_id' => $faker->numberBetween(1,10),
-        //'product_id' => $faker->numberBetween(1,10),
+        'customer_id' => $faker->numberBetween(1,10),
+        'product_id' => $faker->numberBetween(1,10),
 
-        'customer_id' => $faker->unique()->numberBetween($min=1, $max=10),
-        'product_id' => $faker->unique()->numberBetween($min=1, $max=10),
+        //így nem jók:
+        //'customer_id' => $faker->unique()->numberBetween($min=1, $max=10),
+        //'product_id' => $faker->unique()->numberBetween($min=1, $max=10),
 
         'quantity' => $faker->numberBetween(1,20),
         
