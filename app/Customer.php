606826a81d0f1c187c4282daa9dc92298d4e4afa
Bazsmagister <bosmer@mailbox.hu>
2018-02-26 16:52:16 +0100

    models

--- app/Customer.php
+++ app/Customer.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     //public $timestamps = false;
-    protected $fillable= ['customername','address','email'];
+    protected $fillable= ['first_name','last_name','email', 'phone_number', 'zip', 'city', 'street_name', 'street_number'];
+
+   
 
 
     //When defining many-to-many relationships, you use belongsToMany() and not hasMany. hasMany is used for one-to-many relationships.
@@ -16,16 +18,10 @@ class Customer extends Model
        
         //Remember, Eloquent will automatically determine the proper foreign key column on the Comment model. By convention, Eloquent will take the "snake case" name of the owning model and suffix it with _id. So, for this example, Eloquent will assume the foreign key on the Comment model is post_id.
 
-        return  $this->belongsToMany('App\Product');//->withPivot('destination_product')->withTimestamps();
+        return  $this->belongsToMany('App\Product')->withPivot('customer_product')->withTimestamps();
     }
 
-    public function destinations() 
-    {
-        
-        //Remember, Eloquent will automatically determine the proper foreign key column on the Comment model. By convention, Eloquent will take the "snake case" name of the owning model and suffix it with _id. So, for this example, Eloquent will assume the foreign key on the Comment model is post_id.
-
-        return  $this->belongsToMany('App\Desination');//->withPivot('destination_product')->withTimestamps();
-    }
+   
 
 
 }
--- app/Product.php
+++ app/Product.php
@@ -9,18 +9,18 @@ class Product extends Model
     
     //public $timestamps = false;
     //protected $fillable = ['productname', 'price'];
-    protected $fillable = ['productname', 'price', 'VAT'];
+    protected $fillable = ['product_name', 'price', 'VAT', 'ordered_quantity'];
 
-    public static $rules = array(
-        'name' => 'required',
-        'price' => 'required',
-        'VAT' => 'required',
+    // public static $rules = array(
+    //     'name' => 'required',
+    //     'price' => 'required',
+    //     'VAT' => 'required',
 
-    );
+    // );
 
-    public function destinations()
+    public function customers()
     {
-        return  $this->belongsToMany('App\Destination');//->withPivot('destination_product')->withTimestamps();
+        return  $this->belongsToMany('App\Customer')->withPivot('customer_product')->withTimestamps();
 
     }
 
