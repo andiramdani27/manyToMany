427a784db21adb4966d998c6a63310b173d5ca36
Bazsmagister <bosmer@mailbox.hu>
2018-02-26 14:10:03 +0100

    sleep

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -7,9 +7,13 @@ $factory->define(App\Order::class, function (Faker $faker) {
         'customer_id' => $faker->numberBetween(1,10),
         'product_id' => $faker->numberBetween(1,10),
 
+       
         //így nem jók:
         //'customer_id' => $faker->unique()->numberBetween($min=1, $max=10),
         //'product_id' => $faker->unique()->numberBetween($min=1, $max=10),
+        //'customer_id'=> $faker->unique()->randomElement($array = array ('1','2','3','4','5','6','7','8','9','10')),
+        //'product_id'=> $faker->unique()->randomElement($array = array ('2','1','3','4','5','6','7','8','9','10')),
+
 
         'quantity' => $faker->numberBetween(1,20),
         
