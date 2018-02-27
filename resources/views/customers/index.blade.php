23820cbbde65bb23735d4c04b7cd5f9f09c44860
Bazsmagister <bosmer@mailbox.hu>
2018-02-27 10:23:36 +0100

    main.css

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,38 +1,26 @@
-@extends ('layouts.master')
-
-
-
+@extends ('layouts.master') 
 @section('main')
-
-<h1 class="ml-1">Customers</h1>
+<h1 class="ml-1">Vásárlók</h1>
 <div class="row justify-content-center">
 	<ul>
 		@foreach ($customers as $customer)
 
-
 		<li>
-			{{ $customer->last_name }} {{ $customer->first_name }} 
-			<hr style="color:blue;" >
-			Vásárló címe : {{ $customer->street_name }} {{ $customer->street_number }} Irányítószáma: {{ $customer->zip }} Lakóhely :{{ $customer->city
-			}}. Email: {{ $customer->email }} Telefon : {{ $customer->phone_number }}
+			{{ $customer->last_name }} {{ $customer->first_name }}
+			<hr style="color:blue;"> Vásárló címe : <br> Lakóhely :{{ $customer->city }}, {{ $customer->street_name }} {{ $customer->street_number }} <br>Email:
+			{{ $customer->email }} <br>Telefon : {{ $customer->phone_number }}
 			<br>
 			<hr>
-            
 		</li>
-
-        @endforeach
-
-    </ul>
-    
+		@endforeach
+	</ul>
 
 </div>
 @endsection
-
-@section('index') 
-@if (Session::has('message'))
+ 
+@section('index') @if (Session::has('message'))
 <div class="alert alert-info">{{ Session::get('message') }}</div>
 @endif
-
 <table class="table table-striped table-bordered">
 	<thead>
 		<tr>
@@ -45,35 +33,28 @@
 			<td>Házszám</td>
 			<td>Email</td>
 			<td>Telefon</td>
-			
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($customers as $key => $value)
 		<tr>
-			<td>{{ $value->customer_id }}</td>
+			<td>{{ $value->id }}</td>
 			<td>{{ $value->first_name }}</td>
-			<td>{{ $value->last_name }}</td>	
+			<td>{{ $value->last_name }}</td>
 			<td>{{ $value->city }}</td>
 			<td>{{ $value->zip }}</td>
 			<td>{{ $value->street_name }}</td>
 			<td>{{ $value->street_number }}</td>
 			<td>{{ $value->email }}</td>
 			<td>{{ $value->phone_number }}</td>
-			
 
 			<!-- we will also add show, edit, and delete buttons -->
 			<td>
 
-				
 
-			
 				<a class="btn btn-small btn-success" href="{{ URL::to('customers/' . $value->id) }}">Mutasd a vasarlot</a>
 
-				
 				<a class="btn btn-small btn-info" href="{{ URL::to('customers/' . $value->id . '/edit') }}">Vásárlót módosít</a>
-
-					   
 
 			</td>
 		</tr>
@@ -81,6 +62,24 @@
 	</tbody>
 </table>
 @endsection
+ 
+@section('modal')
 
+<div class="grid">
+	<ul>
+		@foreach ($customers as $customer)
 
+		<li>
+			<div class=inner_grid>
+			{{ $customer->last_name }} {{ $customer->first_name }}
+			<hr style="color:blue;"> Vásárló címe : <br> Lakóhely :{{ $customer->city }}, {{ $customer->street_name }} {{ $customer->street_number }} <br>Email:
+			{{ $customer->email }} <br>Telefon : {{ $customer->phone_number }}
+			<br>
+			<hr>
+			</div>
+		</li>
+		@endforeach
+	</ul>
 
+</div>
+@endsection
--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,18 +1,13 @@
 @if (count($errors))
-
 <div class="form-group">
-
-    <div class="alert alert-error"> 
-
-        <ul>
-          @foreach ($errors->all() as $error)
-  
-            <li>      
-              {{$error}}
-            </li>
-          @endforeach
-        </ul>
-    </div>
+  <div class="alert alert-error">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>
+        {{$error}}
+      </li>
+      @endforeach
+    </ul>
+  </div>
 </div>
-
 @endif
